8bf005a2fda0527de360822c886d60ea23dedfc5
hiransarkar21 <mrittunjoysarkar21@gmail.com>
2025-06-24 12:30:10 +0530

    got rid of the backup directory.

--- cluemaster_videoplayer_source_BACKUP/platform_facts.py
+++
@@ -1,3 +0,0 @@
-facts = ["Loading... Loading... Loading",
-         "Loading... Loading... Loading... and more",
-         "Loading... Loading... Loading... and a little more"]
--- cluemaster_videoplayer_source_BACKUP/settings.py
+++
@@ -1,5 +0,0 @@
-import os
-
-# Setting up the base directories for global use
-ROOT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
-MASTER_DIRECTORY = os.path.join(os.environ.get("HOME"), "CluemasterVideoPlayer")
